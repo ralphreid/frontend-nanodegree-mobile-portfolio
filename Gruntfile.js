870c96daf2f7ab3d4b5d1d7de96702ba9f85d39d
Ralph <beresfordjunior@me.com>
2015-04-02 15:05:30 +0100

    set-up page-speed configs for Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,25 @@
-js 'use strict'
+'use strict'
+
 var ngrok = require('ngrok');
+
 module.exports = function(ngrok) {
-// Load grunt tasks require('load-grunt-tasks')(grunt);
+    // Load grunt tasks
+    require('load-grunt-tasks')(grunt);
+    
+    // Grunt configuration
+    grunt.initConfig({
+        pagespeed: {
+            options: {
+                nokey: true,
+                locale: "en_GB",
+                threshold: 40
+            },
+            local: {
+                options: {strategy: "desktop"}
+            },
+            mobile: {
+                options: { strategy: "mobile"}
+            }
+        }
+    });
 };
